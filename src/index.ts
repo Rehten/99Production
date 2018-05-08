b180f3e8806acf796ef087abbce5a625cc713020
Mroow <mroow@yandex.ru>
2018-05-08 18:25:26 +0300

    finished my boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 import './scss/bootstrap.scss'
+import * as $ from 'jquery'
+import 'bootstrap/dist/js/bootstrap.min'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+$(document).on('ready', () => {
+  console.log('App works!');
+});
