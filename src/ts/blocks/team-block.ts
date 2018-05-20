a7f2fdb139aeb3ebc4034a3a6951179d8cc3c315
Mroow <mroow@yandex.ru>
2018-05-20 14:46:02 +0300

    add adaptive design

--- src/ts/blocks/team-block.ts
+++ src/ts/blocks/team-block.ts
@@ -2,5 +2,19 @@ import * as $ from 'jquery'
 import 'owl.carousel'
 
 $(document).ready(function () {
-  $('.team-list').owlCarousel()
+  $('.team-list').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      992: {
+        items: 3,
+        nav: false
+      }
+    }
+  })
 })
