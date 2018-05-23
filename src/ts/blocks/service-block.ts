d83c20145b1f87e77748476880fd19f71454f530
Mroow <mroow@yandex.ru>
2018-05-23 14:25:13 +0300

    add many fixes, and fix services block

--- src/ts/blocks/service-block.ts
+++ src/ts/blocks/service-block.ts
@@ -1,22 +1,24 @@
 import * as $ from 'jquery'
 
-$('.service-item').click(function () {
-  const parameter = $(this).attr('data-target')
-  let isSelected: boolean = $(this).hasClass('active')
+$('.service-items').each(function () {
+  $(this).find('.service-item').click(function () {
+    const parameter = $(this).attr('data-target')
+    let isSelected: boolean = $(this).hasClass('active')
 
-  // made this code more readable
-  if (!isSelected) {
-    // colored selected tab
-    $('.service-item').each(function () {
-      $(this).removeClass('active')
-    })
-    $(this).addClass('active')
+    // made this code more readable
+    if (!isSelected) {
+      // colored selected tab
+      $('.service-item').each(function () {
+        $(this).removeClass('active')
+      })
+      $(this).addClass('active')
 
-  //  show/hide texts and bg-images
-    $('.service-text').removeClass('show')
-    $(`#${parameter}Text`).addClass('show')
+      //  show/hide texts and bg-images
+      $('.service-text').removeClass('show')
+      $(`#${parameter}Text`).addClass('show')
 
-    $('.service-image').removeClass('show')
-    $(`#${parameter}Image`).addClass('show')
-  }
+      $('.service-image').removeClass('show')
+      $(`#${parameter}Image`).addClass('show')
+    }
+  })
 })
